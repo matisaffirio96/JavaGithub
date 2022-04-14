ccbddea59aef4b7e8bf98e7cbde71f62d7f9b155
Mati Saffirio <saffiriomatias@gmail.com>
2022-04-14 14:35:38 -0300

    clone de repo en pc local + creacion de ColaPU

--- impl/ColaLD.java
+++ impl/ColaLD.java
@@ -1,4 +1,30 @@
 package impl;
+import apis.ColaTDA;
 
-public class ColaLD {
+public class ColaLD implements ColaTDA{
+
+    @Override
+    public void inicializarCola() {
+
+    }
+
+    @Override
+    public void acolar(int var1) {
+
+    }
+
+    @Override
+    public void desacolar() {
+
+    }
+
+    @Override
+    public boolean colaVacia() {
+        return false;
+    }
+
+    @Override
+    public int primero() {
+        return 0;
+    }
 }
